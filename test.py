e6099985ad083c48ae618f227d22ba7cc1557eeb
Lukas Bär <l.baer.99@web.de>
2024-09-04 09:51:50 +0200

    implemented swag workflow

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from uncertainty.swag import SWAG
 from gnn.egnn import EGNN
+from datasets.md17_dataset import MD17Dataset
+
+import torch
+from torch import nn
+import time
 
 
 in_node_nf = 12
@@ -7,7 +12,24 @@ in_edge_nf = 0
 hidden_nf = 32
 n_layers = 4
 
+batch_size = 32
+lr = 1e-3
+
+dataset = "datasets/files/ala_converged_forces_1000"
+model_path = None#"./gnn/models/ala_converged_1000000_forces_mve.pt"
+trainset = MD17Dataset(dataset,subtract_self_energies=False, in_unit="kj/mol",train=True, train_ratio=0.2)
+validset = MD17Dataset(dataset,subtract_self_energies=False, in_unit="kj/mol",train=False, train_ratio=0.8)
+
+
+# Create data loaders for train and validation sets
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False)
+validloader = torch.utils.data.DataLoader(validset, batch_size=batch_size, shuffle=False)
 
 swag = SWAG(EGNN, in_node_nf=in_node_nf, in_edge_nf=in_edge_nf, hidden_nf=hidden_nf, n_layers=n_layers)
 
-swag._sample_swag_moments()
+swag.fit(epochs=2, swag_start_epoch=0, swag_freq=1,train_loader=trainloader, valid_loader=validloader, device="cpu", dtype=torch.float32)
+
+#swag.load_state_dict(torch.load("swag.pt"))
+#swag.evaluate_uncertainty(validloader, device="cpu", dtype=torch.float32)
+
--- uncertainty/swag.py
+++ uncertainty/swag.py
@@ -10,13 +10,25 @@ import matplotlib.pyplot as plt
 
 
 class SWAG(nn.Module):
-    def __init__(self, base_model_class, multi_dec = True, *args, **kwargs):
+    def __init__(self, base_model_class, sample_size = 5, low_rank = False,  *args, **kwargs):
         super(SWAG, self).__init__()
-        self.model = base_model_class(*args, **kwargs, multi_dec=multi_dec)
+        self.model = base_model_class(*args, **kwargs)
 
-        self.first_moment = 0
-        self.second_moment = 0
         self.num_samples = 0
+        self.distribution = None
+
+        self.ignore_weight_list = ["first_moment", "second_moment", "cov_matrix"]
+        new_weights = torch.cat([param.view(-1) for param in self.parameters()])
+
+        self.register_buffer("first_moment", torch.zeros_like(new_weights))
+        self.register_buffer("second_moment", torch.zeros_like(new_weights))
+        #self.register_buffer("cov_matrix", torch.zeros(size=(new_weights.shape[0], new_weights.shape[0])))
+
+        
+
+        self.low_rank = low_rank
+
+        self.sample_size = sample_size
 
         self.train_losses_energy = []
         self.train_losses_force = []
@@ -31,41 +43,55 @@ class SWAG(nn.Module):
         self.num_in_interval = 0
         self.total_preds = 0
         self.valid_time = 0
+    
+    def fit(self, epochs, swag_start_epoch, swag_freq, train_loader, valid_loader, device, dtype, model_path="gnn/models/swag.pt" , force_weight=1.0, energy_weight=1.0, log_interval=100):
 
-    def warmup(self, train_loader, optimizer, criterion, device, dtype, epochs = 50, force_weight=1.0, energy_weight=1.0, log_interval=100, num_ensembles=3):
-        print("",flush=True)
-        print("Warmup phase started", flush=True)
-        self.train()
+        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-16)   
+        criterion = nn.L1Loss()
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=200)
+
+        best_valid_loss = np.inf
+
         for epoch in range(epochs):
-            start = time.time()
-            for i,data in enumerate(train_loader):
+            self.train_epoch(train_loader=train_loader, optimizer=optimizer, criterion=criterion, epoch=epoch, device=device, dtype=dtype, force_weight=force_weight, energy_weight=energy_weight, log_interval=log_interval)
+            self.valid_epoch(valid_loader=valid_loader, criterion=criterion, device=device, dtype=dtype, force_weight=force_weight, energy_weight=energy_weight)
+            self.epoch_summary(epoch)
 
-                atom_positions, nodes, edges, atom_mask, edge_mask, label_energy, label_forces, n_nodes = self.prepare_data(data, device, dtype)    
+            if np.array(self.valid_losses_total).mean() < best_valid_loss:
+                best_valid_loss = np.array(self.valid_losses_total).mean()
+                torch.save(self.state_dict(), model_path)
 
-                mean_energy, mean_force, uncertainty = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
+            self.pop_metrics()
+            
+
+            if epoch >= swag_start_epoch and (epoch-swag_start_epoch) % swag_freq == 0:
+                self._sample_swag_moments()
+            else:                                                   # do not change learning rate after starting to sample weights
+                self.lr_before = optimizer.param_groups[0]['lr']
+                scheduler.step(np.array(self.valid_losses_total).mean())
+                self.lr_after = optimizer.param_groups[0]['lr']
+                self.pop_metrics()
+        
+    def predict(self, x, *args, **kwargs):
+        self.eval()
+        coord_shape = x.shape
+        forces = []
+        energies = []
+        
+        for i in range(self.sample_size):
+            self._load_sample_swag_weights()
+            energy, force = self.forward(x, *args, **kwargs)     
+            energies.append(energy.detach().unsqueeze(0))
+            forces.append(force.detach().unsqueeze(0))
+        energies = torch.cat(energies, dim=0)
+        forces = torch.cat(forces, dim=0)
+        uncertainty = torch.std(energies,dim=0)
+        energy = torch.mean(energies, dim=0)
+        force = torch.mean(forces, dim=0)
+        
+        return energy, force, uncertainty
+            
                 
-                loss_energy = criterion(mean_energy, label_energy)
-                loss_force = criterion(mean_force, label_forces)
-                total_loss = force_weight*loss_force + energy_weight*loss_energy
-
-                optimizer.zero_grad()
-                total_loss.backward()
-                #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-                optimizer.step()
-                
-
-                self.train_losses_energy.append(loss_energy.item())
-                self.train_losses_force.append(loss_force.item())
-                self.train_losses_total.append(total_loss.item())
-
-                self.train_uncertainties.append(torch.mean(uncertainty).item())
-                
-                if (i+1) % log_interval == 0:
-                    print(f"Batch {i+1}/{len(train_loader)}, Loss: {loss_energy.item()}, Uncertainty: {uncertainty.item()}", flush=True) 
-            self.train_time = time.time() - start
-            lr = optimizer.param_groups[0]['lr']
-            self.epoch_summary(f"Warmup-{epoch}", False, lr)
-        print("Warmup phase finished", flush=True)
 
     def train_epoch(self, train_loader, optimizer, criterion, epoch, device, dtype, force_weight=1.0, energy_weight=1.0, log_interval=100, num_ensembles=3):
         start = time.time()
@@ -74,12 +100,11 @@ class SWAG(nn.Module):
 
             atom_positions, nodes, edges, atom_mask, edge_mask, label_energy, label_forces, n_nodes = self.prepare_data(data, device, dtype)    
 
-            mean_energy, mean_force, uncertainty = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
+            energy, force = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
             
-            loss_energy = criterion(mean_energy, label_energy)
-            loss_force = criterion(mean_force, label_forces)
+            loss_energy = criterion(energy, label_energy)
+            loss_force = criterion(force, label_forces)
             total_loss = force_weight*loss_force + energy_weight*loss_energy
-            total_loss = 0.5 * torch.mean(torch.log(uncertainty) + total_loss/uncertainty)
 
             optimizer.zero_grad()
             total_loss.backward()
@@ -91,10 +116,9 @@ class SWAG(nn.Module):
             self.train_losses_force.append(loss_force.item())
             self.train_losses_total.append(total_loss.item())
 
-            self.train_uncertainties.append(torch.mean(uncertainty).item())
             
             if (i+1) % log_interval == 0:
-                print(f"Epoch {epoch}, Batch {i+1}/{len(train_loader)}, Loss: {loss_energy.item()}, Uncertainty: {torch.mean(uncertainty).item()}", flush=True)
+                print(f"Epoch {epoch}, Batch {i+1}/{len(train_loader)}, Loss: {loss_energy.item()}", flush=True)
         
         self.train_time = time.time() - start
 
@@ -105,22 +129,16 @@ class SWAG(nn.Module):
         for i,data in enumerate(valid_loader):
             atom_positions, nodes, edges, atom_mask, edge_mask, label_energy, label_forces, n_nodes = self.prepare_data(data, device, dtype)    
 
-            mean_energy, mean_force, uncertainty = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
+            energy, force = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
 
  
-            loss_energy = criterion(mean_energy, label_energy)
-            loss_force = criterion(mean_force, label_forces)
+            loss_energy = criterion(energy, label_energy)
+            loss_force = criterion(force, label_forces)
             total_loss = force_weight*loss_force + energy_weight*loss_energy
-            total_loss = 0.5 * torch.mean(torch.log(uncertainty) + total_loss/uncertainty)
 
             self.valid_losses_energy.append(loss_energy.item())
             self.valid_losses_force.append(loss_force.item())
             self.valid_losses_total.append(total_loss.item())
-
-            self.valid_uncertainties.append(torch.mean(uncertainty).item())
-
-            self.num_in_interval += torch.sum(torch.abs(mean_energy - label_energy) <= uncertainty/2)
-            self.total_preds += mean_energy.size(0)
 
         self.valid_time = time.time() - start
 
@@ -148,16 +166,50 @@ class SWAG(nn.Module):
         
     def forward(self, x, *args, **kwargs):
         output = self.model.forward(x=x, *args, **kwargs)
-        energy = output[:,0].squeeze()
-        variance = output[:,1].squeeze()
+        energy = output.squeeze()
         grad_output = torch.ones_like(energy)
         force = -torch.autograd.grad(outputs=energy, inputs=x, grad_outputs=grad_output, create_graph=True)[0]
-        return energy, force, variance
+        return energy, force
+    
     
     def _sample_swag_moments(self):
-        print(self.parameters())
-        pass
-    
+        params = self.state_dict()
+        
+        
+        new_weights = torch.cat([param.view(-1) for param in self.parameters()])
+
+        
+        if self.num_samples == 0:
+            self.first_moment = torch.zeros_like(new_weights)
+            self.second_moment = torch.zeros_like(new_weights)
+
+        self.first_moment = (self.first_moment * self.num_samples + new_weights) / (self.num_samples + 1)
+        self.second_moment = (self.second_moment * self.num_samples + new_weights**2) / (self.num_samples + 1)
+        self.num_samples += 1
+
+
+    def _load_sample_swag_weights(self):
+        if not self.low_rank:
+            var = torch.clamp(self.second_moment - self.first_moment**2, 1e-30)
+            var_sample = var.sqrt() * torch.randn_like(var)
+            weights = self.first_moment + var_sample
+        else:
+            return NotImplementedError
+            L = torch.linalg.cholesky(self.cov_matrix)
+            z = torch.randn_like(self.first_moment)
+            weights = self.first_moment + L @ z
+        self.load_flattened_weights_into_model(weights)
+        
+    def load_flattened_weights_into_model(self, weights):
+        current_index = 0
+        #flattened_weights = torch.cat([param.view(-1) for param in weights])
+        flattened_weights = weights
+
+        for param in self.parameters():
+            num_param_elements = param.numel()
+            param_flat = flattened_weights[current_index:current_index + num_param_elements]
+            param.data = param_flat.view_as(param).data
+            current_index += num_param_elements
 
     def pop_metrics(self):
         train_losses_energy = self.train_losses_energy
@@ -194,10 +246,10 @@ class SWAG(nn.Module):
         print("", flush=True)
         print(f"Training and Validation Results of Epoch {epoch}:", flush=True)
         print("================================")
-        print(f"Training Loss Energy: {np.array(self.train_losses_energy).mean()}, Training Loss Force: {np.array(self.train_losses_force).mean()}, Training Uncertainty: {np.array(self.train_uncertainties).mean()}, time: {self.train_time}", flush=True)
+        print(f"Training Loss Energy: {np.array(self.train_losses_energy).mean()}, Training Loss Force: {np.array(self.train_losses_force).mean()}, time: {self.train_time}", flush=True)
         if len(self.valid_losses_energy) > 0:
-            print(f"Validation Loss Energy: {np.array(self.valid_losses_energy).mean()}, Validation Loss Force: {np.array(self.valid_losses_force).mean()},Validation Uncertainty: {np.array(self.valid_uncertainties).mean()}, time: {self.valid_time}", flush=True)
-            print(f"Number of predictions within uncertainty interval: {self.num_in_interval}/{self.total_preds} ({self.num_in_interval/self.total_preds*100:.2f}%)", flush=True)
+            print(f"Validation Loss Energy: {np.array(self.valid_losses_energy).mean()}, Validation Loss Force: {np.array(self.valid_losses_force).mean()}, time: {self.valid_time}", flush=True)
+
         print("", flush=True)
 
         if use_wandb:
@@ -222,7 +274,7 @@ class SWAG(nn.Module):
         for i,data in enumerate(test_loader):
             atom_positions, nodes, edges, atom_mask, edge_mask, label_energy, label_forces, n_nodes = self.prepare_data(data, device, dtype)    
 
-            mean_energy, mean_force, uncertainty = self.forward(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
+            mean_energy, mean_force, uncertainty = self.predict(x=atom_positions, h0=nodes, edges=edges, edge_attr=None, node_mask=atom_mask, edge_mask=edge_mask, n_nodes=n_nodes)
             energy_losses = torch.cat((energy_losses, criterion(mean_energy.detach(), label_energy)), dim=0)
             uncertainties = torch.cat((uncertainties, uncertainty.detach()), dim=0)
 
@@ -244,4 +296,4 @@ class SWAG(nn.Module):
 
 
 if __name__ == "__main__":
-    mve = MVE()
+    swag = SWAG(EGNN, in_node_nf=12, in_edge_nf=0, hidden_nf=32, n_layers=4)
